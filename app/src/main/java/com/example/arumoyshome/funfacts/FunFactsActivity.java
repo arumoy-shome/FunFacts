18b07575cbeb8d12e4b3fa6108ff803304db92de
Arumoy Shome <arumoy.shome@gmail.com>
2015-06-06 03:26:54 -0400

    refactor

--- app/src/main/java/com/example/arumoyshome/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/arumoyshome/funfacts/FunFactsActivity.java
@@ -4,35 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 
 public class FunFactsActivity extends ActionBarActivity {
+
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
+        // Declare our view variables and assign them the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                // update the label with our dynamic fact
+                factLabel.setText(fact);
+            }
+        };
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        showFactButton.setOnClickListener(listener);
     }
 }
